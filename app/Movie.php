30a0c1df257de5a46bf910798796f65a8e52f4c1
Andrea <andryx1994@gmail.com>
2020-09-02 12:23:48 +0200

    Versione 0.3
    Aggiunto Model e Migrations

---
+++ app/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+  protected $fillable = [
+    'title',
+    'description',
+    'year',
+    'rating',
+  ];
+}
